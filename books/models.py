868b4e234553c68c1c02e6ee19a2b712f8d8de5e
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-18 12:37:05 +0600

    grapgql

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.TextField()
+    # email = models.EmailField()
+    # image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+
