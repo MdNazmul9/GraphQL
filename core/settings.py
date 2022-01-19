d4330e6f473ec1c88f9f94b1b69e5b728ded44e9
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-19 17:19:06 +0600

    custom user

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Local 
     'books',
     'quiz',
+    'users',
     
     # 3rd party
     'graphene_django',
@@ -80,12 +81,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'graphql_db',
+        'USER': 'nazmul',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT': '5432',
+       }
 }
+
 
 
 # Password validation
@@ -130,3 +143,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.ExtendUser'
