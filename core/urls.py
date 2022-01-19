81e658fa4e1775bb96c9117474a5b88939e0f16d
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-19 19:23:08 +0600

    graphql

--- core/urls.py
+++ core/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
+
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('books.urls')),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+
+    path('books/', include('books.urls')),
     path('quiz/', include('quiz.urls')),
 ]
